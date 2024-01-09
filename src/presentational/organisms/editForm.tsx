2fab3c8bbeb9665363ac4531a717442f2a210d8c
sho_oda <sho_oda@overtech.tokyo>
2024-01-09 17:48:50 +0900

    コードの微調整

--- src/presentational/organisms/editForm.tsx
+++ src/presentational/organisms/editForm.tsx
@@ -1,36 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "../atoms/textField";
 import { Button, Stack } from "@mui/material";
 
 const EditForm: React.FC = () => {
+  const [productData, setProductData] = useState([
+    { label: "商品名", value: "" },
+    { label: "金額", value: "" },
+    { label: "数量", value: "" },
+  ]);
+
+  const handleFieldChange = (index: number, value: string) => {
+    setProductData((prevData) => {
+      const newData = [...prevData];
+      newData[index].value = value;
+      return newData;
+    });
+  };
+
   return (
     <Stack spacing={2}>
-      {/* 商品名 */}
-      <TextField
-        label="商品名"
-        variant="outlined"
-        onChange={function (value: string): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
-
-      {/* 金額 */}
-      <TextField
-        label="金額"
-        variant="outlined"
-        onChange={function (value: string): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
-
-      {/* 数量 */}
-      <TextField
-        label="数量"
-        variant="outlined"
-        onChange={function (value: string): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
+      {productData.map((product, index) => (
+        <TextField
+          key={index}
+          label={product.label}
+          variant="outlined"
+          onChange={(value) => handleFieldChange(index, value)}
+        />
+      ))}
 
       {/* 送信ボタン */}
       <Button variant="contained" color="primary">
